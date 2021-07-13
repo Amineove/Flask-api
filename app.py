a29a6ce00735886a5dcd39f98605d29fb699d3af
Amine <aminoshibatallah@yahoo.fr>
2021-07-13 15:22:05 +0100

    h10

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request
+import sys
+
+from flask import Flask, request, logging
 from flask_restful import Resource, Api, reqparse
 import ast
 import trans
 app = Flask(__name__)
 api = Api(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/')
